07a4adf429ae3977e7aff778092e2fac102368f5
Patrick Haas <patrick.haas@arcusblue.com>
2013-11-28 15:10:32 +0000

    added layout and sign up

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	#new row in db with params form
+  	@user = User.new(user_params)
+
+  	if @user.save #returns true or false based on validations
+  		#only if it passes validations tests
+  		flash[:success] = "You've successfully signed up"
+
+  		#tell the browser, that i am this user. Name user_id created by us
+
+  		session[:user_id] = user.id
+
+  		redirect_to root_path
+  	else
+  		#render same website if it doesn't save
+  		render :new
+  	end
+  end
+
+  # whitelist the users inputs from the form
+  # we want any hackers
+  def user_params
+  	params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Onboard::Application.config.secret_key_base = 'ba313b476541809e7b53c87d17e03bbda5611676a9498c55304db395acc5296cc9003c1c1270e5d600a9e3912094e7cc94aef4a77ef44ac03a3ffbbc0c5430bd'
